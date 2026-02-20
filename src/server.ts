c4ce53673e433da1eee9f1602d138fd7717c5da1
IsraelJorge <israeljorge2000@gmail.com>
2026-02-20 09:53:38 -0300

    feat: Initialize Fastify server with environment variable validation and enhanced linting.

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,17 @@
-console.log('Hello World')
+import { fastify } from 'fastify'
+import {
+  serializerCompiler,
+  validatorCompiler,
+  ZodTypeProvider,
+} from 'fastify-type-provider-zod'
+
+import { ENV } from './shared/env'
+
+const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.setSerializerCompiler(serializerCompiler)
+app.setValidatorCompiler(validatorCompiler)
+
+app
+  .listen({ port: ENV.PORT })
+  .then(() => console.log(`🚀 Server running http://localhost:${ENV.PORT}`))
